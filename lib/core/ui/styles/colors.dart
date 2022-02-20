15e00353670088729e37fb9f072fbec7d44c045b
nzeakokosi7 <nzeakokosi7@gmail.com>
2022-02-20 06:37:21 +0100

    modified user list item widget

--- lib/core/ui/styles/colors.dart
+++ lib/core/ui/styles/colors.dart
@@ -7,4 +7,5 @@ class AppColors {
   static const Color appBackground = Colors.white;
   static const Color titleColor = Colors.black;
   static const Color subTitleColor = Colors.grey;
+  static const Color itemBackground = Color(0xFFF2F2F2);
 }
--- lib/features/user_status/presentation/widgets/user_item_widget.dart
+++ lib/features/user_status/presentation/widgets/user_item_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:wayve_test_app/core/ui/styles/colors.dart';
 import 'package:wayve_test_app/core/ui/styles/text_styles.dart';
 import 'package:wayve_test_app/features/user_status/domain/entities/user_entity.dart';
 import 'package:wayve_test_app/features/user_status/presentation/widgets/user_avatar_widget.dart';
@@ -11,19 +12,25 @@ class UserListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: UserAvatar(
-        id: userEntity.id!,
-        title: userEntity.name!,
-        gender: userEntity.gender!,
-      ),
-      title: Text(
-        userEntity.name!,
-        style: AppTextStyles.mediumTitleTextStyle,
-      ),
-      subtitle: Text(
-        userEntity.email!,
-        style: AppTextStyles.subTitleTextStyle,
+    return Container(
+      color: AppColors.itemBackground,
+      child: ListTile(
+        contentPadding: EdgeInsets.zero,
+        dense: true,
+        minVerticalPadding: 0,
+        leading: UserAvatar(
+          id: userEntity.id!,
+          title: userEntity.name!,
+          gender: userEntity.gender!,
+        ),
+        title: Text(
+          userEntity.name!,
+          style: AppTextStyles.mediumTitleTextStyle,
+        ),
+        subtitle: Text(
+          userEntity.email!,
+          style: AppTextStyles.subTitleTextStyle,
+        ),
       ),
     );
   }

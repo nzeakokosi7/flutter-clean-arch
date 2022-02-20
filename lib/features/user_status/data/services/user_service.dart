f79033bb4814f66ea2d81edc64a0871b99b1a89f
nzeakokosi7 <nzeakokosi7@gmail.com>
2022-02-20 04:52:25 +0100

    setup user service for retrieving users from the api

--- lib/features/user_status/data/services/user_service.dart
+++ lib/features/user_status/data/services/user_service.dart
@@ -1,9 +1,35 @@
 import 'package:flutter/cupertino.dart';
-import 'package:wayve_test_app/features/user_status/data/models/user_response_model.dart';
+import 'package:wayve_test_app/core/api/http_client.dart';
+import 'package:wayve_test_app/core/endpoints/endpoints.dart';
+import 'package:wayve_test_app/core/exceptions/api_request_exception.dart';
 
 abstract class UserService {
-  Future<UserResponseModel> getUsers({
+  Future<dynamic> getUsers({
     required BuildContext context,
-    required route
+    String? route
   });
+}
+
+class UserServiceImpl extends UserService {
+  HttpHelper httpHelper;
+  UserServiceImpl({required this.httpHelper});
+
+
+  @override
+  Future<dynamic> getUsers({
+    required BuildContext context,
+    String? route
+  }) async {
+
+    try {
+      String path = route ?? "";
+      var uri = Endpoints.baseUrl + path;
+
+      final jsonData = await httpHelper.get(uri);
+
+      return jsonData;
+    } catch(e) {
+      throw ApiRequestException(e);
+    }
+  }
 }
